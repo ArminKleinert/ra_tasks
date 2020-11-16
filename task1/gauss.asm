26a1b34e8f67976f5e8eaf7efb65ff265d731657
ArminKleinert <armin.kleinert@t-online.de>
2020-11-16 16:04:27 +0100

    .

--- task1/gauss.asm
+++ task1/gauss.asm
@@ -1,9 +1,8 @@
-; (n * (n + 1)) / 2
           global gauss
 gauss:    mov rax, rdi ; rax = rdi
           add rdi, 1   ; rdi += 1
-          mov rdx, 0   ; rdx = 0
           mul rdi      ; rax *= rdi
+          mov rdx, 0
           mov rcx, 2   ; rcx = 2
           div rcx      ; rax /= rcx
           ret
